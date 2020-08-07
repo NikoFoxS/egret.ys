d162f2238e594738fa44fa8848959027cdfb55c2
niko <niko@bytesuns.com>
2020-08-07 10:30:27 +0800

    新增page和ui

---
+++ src/ys/view/Page.ts
@@ -0,0 +1,36 @@
+namespace ys {
+	export class Page extends ys.UI implements RES.PromiseTaskReporter {
+		public constructor(resGroup: string = '') {
+			super();
+			if (resGroup != '') {
+				if (RES.isGroupLoaded(resGroup)) {
+					this.OnStart();
+				} else {
+					(async () => {
+						await RES.loadGroup(resGroup, 999, this);
+						this.OnStart();
+					})();
+				}
+			} else {
+				this.OnStart();
+			}
+		}
+
+		public cache:boolean;
+
+		onProgress?(current: number, total: number, resItem: RES.ResourceInfo | undefined): void {
+
+		}
+
+		OnStart() {
+
+		}
+	}
+
+	export class PageChangeHandler {
+		public onChange(newView: egret.DisplayObject, oldView: egret.DisplayObject, next: Function): void {
+			//1 页面切换逻辑
+			//2 调用next();结束切换
+		}
+	}
+}
