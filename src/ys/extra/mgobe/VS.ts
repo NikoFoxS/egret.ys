753d179e7c9b6c1280f0f89b916752212d02cb6b
nikofoxs <niko@bytesuns.com>
2020-08-30 16:18:47 +0800

    mgobe

--- src/ys/extra/mgobe/VS.ts
+++ src/ys/extra/mgobe/VS.ts
@@ -32,42 +32,53 @@ namespace ys {
 
 		private onJoinRoom(event: MGOBE.types.BroadcastEvent<MGOBE.types.JoinRoomBst>) {
 			console.log("新玩家加入", event);
+			this.dispatchEventWith(VS.ON_PLAYER_JOIN,false,event.data);
 		}
 
 		private onLeaveRoom(event: MGOBE.types.BroadcastEvent<MGOBE.types.LeaveRoomBst>) {
 			console.log("玩家退出", this.room.roomInfo);
+			this.dispatchEventWith(VS.ON_PLAYER_LEAVE,false,event.data);
 		}
 
 		private onUpdate(room?: MGOBE.Room) {
 			if (!room.networkState.COMMON) {
 				console.log('自己网络断开了')
+				this.dispatchEventWith(VS.ON_DISCONNECT);
 			}
 		}
 
 		private onChangePlayerNetworkState(event: MGOBE.types.BroadcastEvent<MGOBE.types.ChangePlayerNetworkStateBst>) {
-			// console.log('玩家网络状态变化', event)
 			if (event.data.networkState === MGOBE.ENUM.NetworkState.COMMON_OFFLINE) {
 				console.log("玩家掉线", event);
+				this.dispatchEventWith(VS.ON_PLAYER_OFFLINE, false, event.data);
 			}
 
 		}
 
 		private onRecvFromClient(event: MGOBE.types.BroadcastEvent<MGOBE.types.RecvFromClientBst>) {
 			// console.log('onRecvFromClient', event);
-			let data = event.data as MGOBE.types.RecvFromClientBst;
-			console.log('onRecvFromClient::', data);
+			this.dispatchEventWith(VS.ON_MSG_FROM_CLIENT, false, event.data);
 		}
 
 		private onRecvFromGameSvr(event: MGOBE.types.BroadcastEvent<MGOBE.types.RecvFromGameSvrBst>) {
-
+			this.dispatchEventWith(VS.ON_MSG_FROM_SERVER, false, event.data);
 		}
 
 		static ON_CONNNECT: string = 'on_connect';
+		static ON_DISCONNECT: string = 'on_disconnect';
+
 		static ON_MATCH_OK: string = 'on_match_ok';
 		static ON_MATCH_KO: string = 'on_match_ko';
 
 		static ON_JOIN_OK: string = 'on_join_ok';
 		static ON_JOIN_KO: string = 'on_join_ko';
+
+		static ON_MSG_FROM_CLIENT: string = 'on_msg_from_client';
+		static ON_MSG_FROM_SERVER: string = 'on_msg_from_server';
+
+		static ON_PLAYER_OFFLINE: string = 'on_player_offline';
+		static ON_PLAYER_JOIN: string = 'on_player_join';
+		static ON_PLAYER_LEAVE: string = 'on_player_leave';
 
 		private gameInfo;
 		private room: MGOBE.Room;
@@ -77,6 +88,7 @@ namespace ys {
 				gameId: gameId,// 替换为控制台上的“游戏ID”
 				secretKey: secretKey,// 替换为控制台上的“游戏key”
 			}
+			console.log('openid', openId)
 		}
 
 		private _serverTime: number;
@@ -131,52 +143,91 @@ namespace ys {
 		}
 
 		/**io类快速游戏 */
-		joinPlay(playerInfo, maxPlayers: number, roomType: string) {
+		joinPlay(playerInfo, maxPlayers: number, roomType: string, leaveRoom = false) {
 			console.log('检查玩家是否已经加房')
 			MGOBE.Room.getMyRoom(event => {
 				if (event.code === 0) {
 					// 设置房间信息到 room 实例
 					this.room.initRoom(event.data.roomInfo);
+					if (leaveRoom) {
+						this.leaveRoom(() => {
+							this.matchRoom(playerInfo, maxPlayers, roomType);
+						}, this);
+					} else {
+						this.dispatchEventWith(ys.VS.ON_JOIN_OK, false, event.data.roomInfo.playerList);
+					}
+
 					return console.log("玩家已在房间内：", event.data.roomInfo);
 				}
 
 				console.log("玩家不在房间内");
 				if (event.code === 20011) {
-					const matchRoomPara = {
-						playerInfo: playerInfo,
-						maxPlayers: maxPlayers,
-						roomType: roomType
-					}
-					console.log('快速加房', playerInfo, maxPlayers, roomType);
-					this.room.matchRoom(matchRoomPara, event => {
-
-						if (event.code == 0) {
-							console.log("加房成功", event);
-							this.dispatchEventWith(ys.VS.ON_JOIN_OK,false,event.data.roomInfo.playerList);
-						} else {
-							console.log("匹配失败", event);
-							this.dispatchEventWith(ys.VS.ON_JOIN_KO);
-						}
-					});
-
+					this.matchRoom(playerInfo, maxPlayers, roomType);
 				}
-
 			});
 
 
 		}
-		/**匹配类游戏 */
-		matchPlay() {
 
+		private matchRoom(playerInfo, maxPlayers: number, roomType: string) {
+			const matchRoomPara = {
+				playerInfo: playerInfo,
+				maxPlayers: maxPlayers,
+				roomType: roomType
+			}
+			console.log('快速加房', playerInfo, maxPlayers, roomType);
+			this.room.matchRoom(matchRoomPara, event => {
+
+				if (event.code == 0) {
+					console.log("加房成功", event);
+					this.dispatchEventWith(ys.VS.ON_JOIN_OK, false, event.data.roomInfo.playerList);
+				} else {
+					console.log("匹配失败", event);
+					this.dispatchEventWith(ys.VS.ON_JOIN_KO);
+				}
+			});
 		}
 
-		leaveRoom() {
+		/**匹配类游戏，根据后台设置的匹配规则进行匹配 */
+		matchPlay(playerInfo, matchCode) {
+			const matchPlayersPara = {
+				playerInfo,
+				matchCode: matchCode,
+			};
+			let room = this.room;
+
+			// 监听匹配结果
+			MGOBE.Room.onMatch = (event) => {
+
+				if (event.data.errCode === MGOBE.ErrCode.EC_OK) {
+					console.log("组队匹配成功");
+					room.initRoom(event.data.roomInfo);
+					this.dispatchEventWith(ys.VS.ON_MATCH_OK, false, event.data.roomInfo.playerList);
+				} else {
+					console.log("组队匹配失败");
+					this.dispatchEventWith(ys.VS.ON_MATCH_KO);
+				}
+			};
+
+			// 发起匹配
+			room.matchPlayers(matchPlayersPara, event => {
+				if (event.code === 0) {
+					// console.log("请求成功");
+				} else {
+					// console.log("请求失败", event.code);
+					this.dispatchEventWith(ys.VS.ON_MATCH_KO);
+				}
+			});
+		}
+
+		leaveRoom(callback: Function, ref: any) {
 			this.room.leaveRoom({}, event => {
 				if (event.code === 0) {
 					// 退房成功
 					console.log("退房成功", event.data.roomInfo);
 					// 可以使用 initRoom 清除 roomInfo
 					this.room.initRoom();
+					callback.call(ref);
 				}
 			});
 		}
